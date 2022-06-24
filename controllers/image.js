22137ce4d7c2e4cc7e539ed2ac24a182085e3484
Rayan <53921750+binelias@users.noreply.github.com>
2022-06-24 20:42:47 +0800

    move clarifai JSREST back to frontend

--- controllers/image.js
+++ controllers/image.js
@@ -1,35 +1,3 @@
-const handleApiCall = (req, res) => {
-    const raw = JSON.stringify({
-        "user_app_id": {
-            "user_id": "binray",
-            "app_id": "iProfiler"
-        },
-        "inputs": [
-            {
-                "data": {
-                    "image": {
-                    "url": req.body.input
-                    }
-                }
-            }
-        ]
-    });
-
-    await fetch("https://api.clarifai.com/v2/models/face-detection/versions/45fb9a671625463fa646c3523a3087d5/outputs", {
-        method: "POST",
-        headers: {
-                Accept: "application/json",
-                Authorization: 'Key a13ffff405e243a99bde0d98a85e6170'
-            },
-            body:raw,
-    })
-    .then(response => response.text())
-    .then(data => {
-        res.json(data);
-    })
-    .catch(err => res.status(400).json('unable to work with API'))
-}
-
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -42,6 +10,5 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage,
-    handleApiCall
+    handleImage
 };
--- server.js
+++ server.js
@@ -31,7 +31,6 @@ app.post('/login', login.handleLogin(db, bcrypt))
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`App is running on port ${process.env.PORT}`)
