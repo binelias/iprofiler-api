51ab2d3d5c0097c893ed0707b83373200c6bf8e7
Rayan <53921750+binelias@users.noreply.github.com>
2022-06-26 12:42:12 +0800

    adding local psql database for reference

--- server.js
+++ server.js
@@ -19,6 +19,17 @@ const db = knex({
     }
 });
 
+// const db = knex({
+//     client: 'pg',
+//     version: '7.2',
+//     connection: {
+//         host : '127.0.0.1',
+//         port: 5432,
+//         user : 'postgres',
+//         password : 'test',
+//         database : 'iprofiler',
+//     }
+// });
 
 
 const app = express();
